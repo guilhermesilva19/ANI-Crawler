b6f1c6f5e2c43dbc6fe350964853aaa3ea3bd9dc
AbdulwahidHusein <abdulwahidhussen750@gmail.com>
2025-07-18 18:38:44 +0300

    added config

--- configes.py
+++ configes.py
@@ -133,4 +133,4 @@ TARGET_URLS = [
 
 MONGODB_URI = os.getenv('MONGODB_URI')  # mongodb+srv://username:password@cluster.mongodb.net/
 SITE_ID = os.getenv('SITE_ID', 'health_gov_au')  # Unique identifier for this site
-SITE_NAME = os.getenv('SITE_NAME', 'Department of Health')  # Human-readable site name'''
+SITE_NAME = os.getenv('SITE_NAME', 'Department of Health')  # Human-readable site name'''                                                                                                                                                                                                                   
