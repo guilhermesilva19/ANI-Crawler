2736a8d6f67be8fb70434a1a99076901c40f4f1d
Alex <alexaariass92@gmail.com>
2025-07-24 10:07:58 -0400

    update/check memory limit

--- src/config.py
+++ src/config.py
@@ -38,9 +38,6 @@ EXCLUDE_PREFIXES = [
     "https://www.education.gov.au/news"
 ]
 
-# HTML-only filtering configuration
-HTML_ONLY_MODE = os.getenv('HTML_ONLY_MODE', 'true').lower() == 'true'  # Set to 'false' to include all file types
-
 
 # Browser Configuration
 CHROME_OPTIONS = {
